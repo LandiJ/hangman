0adcb005404ea159c9920c6d8ac6dbcb74e8cbf1
Alandryia Jones <jonesalandryia@gmail.com>
2017-06-23 08:22:36 -0500

    try again button works

--- server.js
+++ server.js
@@ -33,8 +33,24 @@ var spaces = [];
 var guesses = [];
 var wrong = [];
 var right = [];
+var lose;
+var rightWord;
 
 app.get("/", function(req, res) {
+  // console.log(randomWord.length);
+  // theWord = randomWord.split("");
+  // console.log(theWord);
+  // for (var i = 0; i < theWord.length; i++) {
+  //   spaces.push("_____");
+  // }
+  // console.log(spaces);
+  res.render("index");
+});
+
+app.get("/play", function(req, res) {
+  spaces = [];
+  randomWord = "";
+  randomWord = words[Math.floor(Math.random() * words.length)];
   console.log(randomWord.length);
   theWord = randomWord.split("");
   console.log(theWord);
@@ -42,28 +58,45 @@ app.get("/", function(req, res) {
     spaces.push("_____");
   }
   console.log(spaces);
-  res.render("index");
+
+  res.render("play", { spaces: spaces });
 });
 
-app.get("/play", function(req, res) {
-  res.render("play", { spaces: spaces });
+app.get("/again", function(req, res) {
+  res.redirect("/play");
 });
 
 app.post("/guess", function(req, res) {
   var guess = req.body.guess;
-  guesses.push(guess);
-  console.log(req.body.guess);
-  if (theWord.indexOf(guess) > -1) {
-    right.push(guess);
+  if (wrong.length < 8) {
+    guesses.push(guess);
+    console.log(req.body.guess);
+    if (theWord.indexOf(guess) > -1) {
+      right.push(guess);
+      for (var i = 0; i < theWord.length; i++) {
+        if (theWord[i] == guess) {
+          console.log(i + 1);
+          spaces[i] = theWord[i];
+        }
+      }
+    } else {
+      wrong.push(guess);
+    }
+  } else {
+    console.log("you lose");
+    lose = true;
+    rightWord = theWord;
     for (var i = 0; i < theWord.length; i++) {
-      if (theWord[i] == guess) {
+      if (theWord[i] == rightWord[i]) {
         console.log(i + 1);
         spaces[i] = theWord[i];
       }
     }
   }
 
-  res.render("play", { guesses: guesses, spaces: spaces });
+  console.log(wrong);
+
+  res.render("play", { guesses: guesses, spaces: spaces, lose: lose });
 });
 
 app.listen(port, function() {
