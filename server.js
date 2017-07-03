cbaebc4208cb3e17bf14396e2e8c2253c8d343fa
Alandryia Jones <jonesalandryia@gmail.com>
2017-07-03 07:23:48 -0500

    added validations

--- server.js
+++ server.js
@@ -19,13 +19,6 @@ app.use("/", express.static("./public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session(sessionConfig));
-function checkAuth(req, res, next) {
-  if (!req.session.user) {
-    return res.redirect("/login");
-  } else {
-    next();
-  }
-}
 
 var randomWord = words[Math.floor(Math.random() * words.length)];
 var theWord;
@@ -33,24 +26,27 @@ var spaces = [];
 var guesses = [];
 var wrong = [];
 var right = [];
+var win;
 var lose;
 var rightWord;
+var tooLong = false;
 var tries = "8";
 
 app.get("/", function(req, res) {
-  // console.log(randomWord.length);
-  // theWord = randomWord.split("");
-  // console.log(theWord);
-  // for (var i = 0; i < theWord.length; i++) {
-  //   spaces.push("_____");
-  // }
-  // console.log(spaces);
   res.render("index");
 });
 
 app.get("/play", function(req, res) {
+  console.log(lose);
   spaces = [];
+  wrong = [];
+  right = [];
+  guesses = [];
+  lose = false;
+  var rightWord;
+  tries = "8";
   randomWord = "";
+  console.log(req.body);
   randomWord = words[Math.floor(Math.random() * words.length)];
   console.log(randomWord.length);
   theWord = randomWord.split("");
@@ -59,17 +55,24 @@ app.get("/play", function(req, res) {
     spaces.push("_____");
   }
   console.log(spaces);
+  console.log(req.session);
 
   res.render("play", { spaces: spaces });
 });
 
 app.get("/again", function(req, res) {
+  req.session.destroy();
   res.redirect("/play");
+  lose = false;
+  console.log(lose);
 });
 
 app.post("/guess", function(req, res) {
   var guess = req.body.guess;
-  if (wrong.length < 7) {
+  if (guess.length > 1) {
+    return;
+  }
+  if (wrong.length < 7 && guess.length == 1) {
     guesses.push(guess);
     console.log(req.body.guess);
     if (theWord.indexOf(guess) > -1) {
@@ -78,6 +81,12 @@ app.post("/guess", function(req, res) {
         if (theWord[i] == guess) {
           console.log(i + 1);
           spaces[i] = theWord[i];
+          console.log(spaces);
+          console.log(theWord);
+        }
+        if (spaces.toString() == theWord.toString()) {
+          console.log("you win!");
+          var win = true;
         }
       }
     } else {
@@ -103,7 +112,8 @@ app.post("/guess", function(req, res) {
     guesses: guesses,
     spaces: spaces,
     lose: lose,
-    tries: tries
+    tries: tries,
+    win: win
   });
 });
 
